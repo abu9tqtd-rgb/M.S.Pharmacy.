81bd053a976b83d446b8242cc9452fdfb6345b6f
Abubakar Shaik <abu9tqtd@gmail.com>
2026-06-13 01:29:57 +1300

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,9 @@
 const products = [
 
 {
-name:"All Nestle product are available",
-price:"Available
-image:"https://i.ibb.co/1G2wX4m5/file-0000000022547209a55ad6ace5b58f9a.png"
+name:"Paracetamol 500mg",
+price:30,
+image:"https://images.unsplash.com/photo-1584308666744-24d5c474f2ae?w=600"
 },
 
 {
